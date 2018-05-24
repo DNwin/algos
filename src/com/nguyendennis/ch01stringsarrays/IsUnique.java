93161b8f2bead7a734f56db105999b605194e7d5
Dennis Nguyen <ndennis@gmail.com>
2018-05-24 00:33:52 -0700

    Strings and Arrays - isUnique using bitmask

--- src/com/nguyendennis/ch01stringsarrays/IsUnique.java
+++ src/com/nguyendennis/ch01stringsarrays/IsUnique.java
@@ -11,17 +11,44 @@ public class IsUnique {
        assert !isUnique("hello") : "not unique";
        assert isUnique("asdf") : "unique";
        assert isUnique("") : "unique";
+
+       assert !isUniqueBitMask("hello") : "not unique";
+       assert isUniqueBitMask("asdf") : "unique";
+       assert isUniqueBitMask("") : "unique";
     }
 
+    /**
+     * Use of extra data structure.
+     * Runtime: O(n)
+     * @param str
+     * @return
+     */
     static boolean isUnique(String str) {
         HashSet<Character> characterSet = new HashSet<>();
-
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (characterSet.contains(c)) {
                 return false;
             }
             characterSet.add(c);
+        }
+        return true;
+    }
+
+    /**
+     * Use of bitmask to avoid extra data structure
+     * Runtime: O(n)
+     * @param str
+     * @return
+     */
+    static boolean isUniqueBitMask(String str) {
+        int bitMask = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if ((bitMask & (1 << c)) > 0) {
+                return false;
+            }
+            bitMask |= (1 << c);
         }
         return true;
     }
